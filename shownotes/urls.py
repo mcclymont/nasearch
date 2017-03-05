5c5a98d3ab1e105ccc3a767809879dca1de0fa3c
Chris McClymont <chris@mcclymont.it>
2017-03-05 18:04:32 +1100

    Remove deprecated methods from urls.py

--- shownotes/urls.py
+++ shownotes/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from shownotes import views
 from shownotes import api
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', views.index),
     url(r'^search$', views.search_topics),
     url(r'^api/topics$', api.topics),
     url(r'^api/search$', api.search),
     url(r'^api/show$', api.show),
     url(r'^api/note$', api.note)
-)
+]
